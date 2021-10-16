d15dc2a5d6fce4465a2bc10a06fb0cd99abf436b
Robert <robert.rachubinski@gmail.com>
2021-10-16 22:51:37 +0200

    Witryna obsługuje tylko jeden element listy.

--- functional_test.py
+++ functional_test.py
@@ -30,26 +30,35 @@ class NewVisitorTest(unittest.TestCase):
 
         # W plu tekstowym wpisała "Kupić pawie pióra"
         # (hobby Edyty polega na tworzeniu ozdobnych przynęt)
-        inputbox.send_keys('Kupić pawie pióra')        
+        inputbox.send_keys('Kupić pawie pióra')
 
         # Po naciśnięciu klawisza Enter strona została uaktalniona i wyświela
         # "1: Kupić pawie pióra" jako element listy rzeczy do zrobienia.
         inputbox.send_keys(Keys.ENTER)
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Kupić pawie pióra' for row in rows),
-            "Nowy element nie znajduje się w tabeli."
-        )
+
+        rows = self.browser.find_elements_by_tag_name('tr')
+	### W książce było jak niżej, ale to nie działało:
+        ### rows = table.find_elements_by_tag_name('tr')
+
+        self.assertIn('1: Kupić pawie pióra', [row.text for row in rows])
 
         # Na stronie nadal znajduje się pole tekstowe zachęcające do podania kolejnego zadania.
         # Edyta wpisała "Zrobić przynęty z pawich piór"
-        self.fail('Zakończenie testu!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Zrobić przynęty z pawich piór')
+        inputbox.send_keys(Keys.ENTER)
 
         # Strona została ponownie auaktualniona i teraz wyświetla dwa elementy na liście rzeczy do zrobienia.
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = self.browser.find_elements_by_tag_name('tr')
+        self.assertIn('1: Kupić pawie pióra', [row.text for row in rows])
+        self.assertIn('2: Zrobić przynęty z pawich piór', [row.text for row in rows])
 
-        # Edyta była ciekawa, czy witryna zapamięta jej listę. Zwróciłą uwagę na wygenerowany dla niej
-        # unikatowy adres URL, obok którego znajduje się pewien tekst z wyjaśnieniem.
+        # Edyta była ciekawa, czy witryna zapamięta jej listę. 
+	# Zwróciłą uwagę na wygenerowany dla niej unikatowy adres URL, 
+	# obok którego znajduje się pewien tekst z wyjaśnieniem.
+        self.fail('Zakończenie testu!')
 
         # Przechodzi pod podany adres URL i widzi wyświetloną swoją listę rzeczy do zrobienia.
 
