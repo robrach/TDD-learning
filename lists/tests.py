fe9336dff3e9567dc8a39d2735797f682daed5fa
Robert <robert.rachubinski@gmail.com>
2021-10-18 20:45:05 +0200

    Przekierowanie po żądaniu POST, wyświetlanie wszystkich elementów w szablonie

--- lists/tests.py
+++ lists/tests.py
@@ -7,7 +7,7 @@ from django.http import HttpRequest
 from lists.views import home_page
 
 class HomePageTest(TestCase):
-    
+
     def test_root_url_resolve_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
@@ -18,20 +18,41 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
 
-    def  test_home_page_can_save_a_POST_request(self):
+    def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
         request.method = 'POST'
         request.POST['item_text'] = 'Nowy element listy'
 
         response = home_page(request)
-        
-        self.assertIn('Nowy element listy', response.content.decode())
-        expected_html = render_to_string(
-                'home.html',
-                {'new_item_text': 'Nowy element listy'}
-        )
-        self.assertEqual(response.content.decode(), expected_html)
 
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'Nowy element listy')
+
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'Nowy element listy'
+
+        response = home_page(request)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_home_page_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
 
 class ItemModelTest(TestCase):
 
