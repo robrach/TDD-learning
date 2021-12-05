9637c7b5c674023a47b2fd6b0e455a6834870f94
Robert <robert.rachubinski@gmail.com>
2021-12-05 19:40:43 +0100

    Nowe widoki i poprawiony błąd w widoku 'new_list'.

--- lists/tests.py
+++ lists/tests.py
@@ -106,4 +106,31 @@ class NewListTest(TestCase):
             '/lists/new',
             data={'item_text': 'Nowy element listy'}
         )
-        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+        new_list = List.objects.first()
+        self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
+
+
+class NewItemTest(TestCase):
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            'lists/%d/add_item' % (correct_list.id,),
+            data={'item_text': 'Nowy element dla istniejącej listy'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'Nowy element dla istniejącej listy')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data={'item_text': 'Nowy element dla istniejącej listy'}
+        )
+        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
